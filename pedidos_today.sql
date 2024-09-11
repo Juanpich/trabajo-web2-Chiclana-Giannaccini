a4abf67ecd3ad652e4ae193dfbb78a36e90e574a
juanpi chiclana <juanpachiclanaurraco@gmail.com>
2024-09-11 19:22:53 -0300

    Cargo el archivo .sql

---
+++ pedidos_today.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-09-2024 a las 00:19:30
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pedidos_today`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidios`
+--
+
+CREATE TABLE `pedidios` (
+  `id` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad_productos` int(11) NOT NULL,
+  `total` int(100) NOT NULL,
+  `fecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `pedidios`
+--
+
+INSERT INTO `pedidios` (`id`, `id_producto`, `cantidad_productos`, `total`, `fecha`) VALUES
+(1, 1, 2, 4000, '2024-09-11'),
+(3, 2, 1, 3000, '2024-09-19'),
+(6, 5, 3, 1500, '2024-09-12');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `precio` double NOT NULL,
+  `descripcion` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id`, `nombre`, `precio`, `descripcion`) VALUES
+(1, 'Hamburguesa doble con chedar', 2000, 'Habmurguesa doble carne, con chedar, huevo, tomate, lechuga.'),
+(2, 'Pizza mozzarella', 3000, 'Pizza con salsa de tomate y mucha mozzarella'),
+(5, 'Papas', 500, 'Papas artesanalmente recolectadas, cortadas y fritas');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `pedidios`
+--
+ALTER TABLE `pedidios`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id_producto`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pedidios`
+--
+ALTER TABLE `pedidios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `pedidios`
+--
+ALTER TABLE `pedidios`
+  ADD CONSTRAINT `pedidios_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
