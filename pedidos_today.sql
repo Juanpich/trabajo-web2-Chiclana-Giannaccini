f09bf63d939ed6b0fde9f5ba5954ab505696ed2a
MARIA GIANA <mariajosegiana@gmail.com>
2024-10-07 11:31:14 -0300

    base de datos actualizada

--- pedidos_today.sql
+++ pedidos_today.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-10-2024 a las 19:49:28
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 07-10-2024 a las 16:24:52
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -40,11 +40,10 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `id_product`, `cant_products`, `total`, `date`) VALUES
-(11, 6, 5, 500, '2024-10-03'),
-(12, 6, 2, 200, '2024-10-03'),
-(13, 2, 2, 6000, '2024-10-12'),
-(15, 5, 6, 3000, '2024-10-08'),
-(16, 7, 4, 2000, '2024-10-28');
+(1, 1, 2, 4000, '2024-09-11'),
+(3, 2, 1, 3000, '2024-09-19'),
+(6, 5, 3, 1500, '2024-09-12'),
+(7, 1, 1, 2000, '2024-09-30');
 
 -- --------------------------------------------------------
 
@@ -56,18 +55,20 @@ CREATE TABLE `product` (
   `id` int(11) NOT NULL,
   `name` varchar(100) DEFAULT NULL,
   `price` double DEFAULT NULL,
-  `description` varchar(150) DEFAULT NULL
+  `description` varchar(150) DEFAULT NULL,
+  `image_product` varchar(120) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `product`
 --
 
-INSERT INTO `product` (`id`, `name`, `price`, `description`) VALUES
-(2, 'Pizza mozzarella', 3000, 'Pizza con salsa de tomate y mucha mozzarella'),
-(5, 'Papas', 500, 'Papas artesanalmente recolectadas, cortadas y fritas'),
-(6, 'Coca Cola', 100, 'Bebida muy refrescante'),
-(7, 'Bombon helado', 500, 'postre bombon helado de alta calidad y el mejor sabor a chocolate');
+INSERT INTO `product` (`id`, `name`, `price`, `description`, `image_product`) VALUES
+(1, 'Hamburguesa doble con chedar', 3000, 'Hamburguesa doble carne, con chedar, huevo, tomate, lechuga.', 'https://www.carniceriademadrid.es/wp-content/uploads/2022/09/smash-burger-que-es.jpg'),
+(2, 'Pizza mozzarella', 3000, 'Pizza con salsa de tomate y mucha mozzarella', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI2hdQeNVlyu20ReOpJcNwdgW0ER5hwxnauQ&amp;s'),
+(5, 'Papas', 500, 'Papas artesanalmente recolectadas, cortadas y fritas', NULL),
+(6, 'Picada', 6000, 'Salamin, quesos y aceitunas', NULL),
+(7, 'Limonada', 1000, 'Jugo fresco de limones exprimidos, genjibre y azucar', 'https://cdn0.celebritax.com/sites/default/files/styles/amp/public/recetas/limonada.jpg');
 
 -- --------------------------------------------------------
 
@@ -77,16 +78,16 @@ INSERT INTO `product` (`id`, `name`, `price`, `description`) VALUES
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `userName` varchar(250) NOT NULL,
-  `password` char(60) NOT NULL
+  `user_name` varchar(100) NOT NULL,
+  `password` varchar(120) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`id`, `userName`, `password`) VALUES
-(1, 'juanpich', '$2y$10$WgfdVcd3ntADRG93D0Byduz5gRpdw7/QzPkdKG.GAbJqVqTXlMgUO');
+INSERT INTO `user` (`id`, `user_name`, `password`) VALUES
+(1, 'webadmin', '$2y$10$r8HJhVApm4gkltB2xy0tyu./QIdz3cSprUj/yk6K6MUsNn8TT7w/O');
 
 --
 -- Índices para tablas volcadas
@@ -109,8 +110,7 @@ ALTER TABLE `product`
 -- Indices de la tabla `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `userName` (`userName`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -120,7 +120,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT de la tabla `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `product`
@@ -132,7 +132,7 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
